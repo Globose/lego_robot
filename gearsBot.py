fd85fcdaeb4a1e72ea76d3676c7c3a958e98f3ab
Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
2022-10-26 18:19:23 +0200

    Update gearsBot.py

--- gearsBot.py
+++ gearsBot.py
@@ -24,49 +24,62 @@ right_light = ColorSensor(Port.S3)
 
 # Here is where your code starts
 
+def standardize(color_left, color_right, color_current):
+    """Returns a number between -1 and 1"""
+    t = (color_current-min(color_left, color_right) -
+         .5*abs(color_left-color_right))
+    return (2*t)/(color_left-color_right)
+
+
+def velocity_fn(x, base_velocity, steering_offset):
+    """Calculates left and right velocity"""
+    velocity_left = (min(base_velocity, base_velocity+2*base_velocity*x) +
+                     x*base_velocity*min(steering_offset, 0))
+    velocity_right = (min(base_velocity, base_velocity-2*base_velocity*x) +
+                      x*base_velocity*max(steering_offset, 0))
+    return (velocity_left, velocity_right)
+
+
 def drive_robot(velocity):
     """Drives robot forward"""
     left_motor.run(speed=velocity[0])
     right_motor.run(speed=velocity[1])
 
-def calc_velocity(color_line, color_base, color_current, steering_offset, drive_side, base_velocity):
-    """Returns a tuple with right and left velocity"""
-    f = (color_current-min(color_line,color_base)-
-        .5*abs(color_line-color_base))/(.5*(color_line-color_base))
-    f *= drive_side
-    f = min(f,1)
-    f = max(f,-1)
-    
-    #print(f)
-    if f > 0:
-        return (base_velocity, base_velocity-2*f*base_velocity)
-    return (base_velocity+2*f*base_velocity, base_velocity)
 
-def drive_forward(color_line, color_base, velocity):
-    """Drives robot forward until on the line"""
+def get_color_furthest(color_left, color_right, color_current):
+    """Returns the color furthest away from color_current"""
+    if abs(color_current-color_left) > abs(color_current-color_right):
+        return color_right
+    return color_left
+
+
+def drive_forward(color_left, color_right, velocity):
+    """Drives robot forward until it has passed the line"""
     drive_robot(velocity)
-    difference = abs(color_base-color_line)
+    color_current = left_light.reflection()
+    color_target = get_color_furthest(color_left, color_right, color_current)
+    distance = abs(color_current-color_target)
     while True:
         color_current = left_light.reflection()
-        if color_line - difference/4 < color_current < color_line + difference/4:
+        new_distance = abs(color_current-color_target)
+        if new_distance > distance:
             break
+        wait(200)
+    # ev wait(300)
+
 
 def main():
     """Main Function"""
-    color_line, color_base = 0, 100
+    color_left, color_right = 0, 100
     base_velocity = 200
     steering_offset = 1
-    drive_side = 1 # -1 = left side, 1 = right side
-
-    drive_forward(color_line, color_base, (base_velocity,base_velocity))
-
-    #print(calc_velocity(color_line, color_base, 80, steering_offset, drive_side, base_velocity))
+    drive_forward(color_left, color_right, (base_velocity, base_velocity))
 
     while True:
-        drive_robot(calc_velocity(color_line, color_base, 
-            left_light.reflection(), steering_offset, drive_side, base_velocity))
+        s = standardize(color_left, color_right, left_light.reflection())
+        velocity = velocity_fn(s, base_velocity, steering_offset)
+        drive_robot(velocity)
+
 
 if __name__ == '__main__':
     main()
-
-
