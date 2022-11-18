3550b70645146ccc4a7e308a8cab24739ac5c249
Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
2022-11-18 17:49:12 +0100

    bluetooth enabled

--- robot/main.py
+++ robot/main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env pybricks-micropython
 import time
+import random
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
-from pybricks.parameters import Port
+from pybricks.parameters import Port, Color
 from pybricks.tools import wait
+from pybricks.messaging import BluetoothMailboxServer, TextMailbox
 
 # Robot definition
 ev3 = EV3Brick()
@@ -27,7 +29,7 @@ def norm(color_left, color_right, color_current):
     t = (color_current-min(color_left, color_right) -
          .5*abs(color_left-color_right))
     t = (2*t)/(color_left-color_right)
-    t = 0.6*(t**3+t)/2
+    t = 0.88*(t**3+t)/2
     return t
 
 
@@ -76,7 +78,7 @@ def park():
     drive_robot((0, 0))
 
 
-def parking_mode(color_line, color_base):
+def parking_mode(color_line, color_base, mbox):
     """Attempts to park the robot"""
     drive_robot(velocity_fn(-1, 150, -1))
     distances = []
@@ -91,11 +93,28 @@ def parking_mode(color_line, color_base):
 
     if parking_empty:
         park()
-        wait(5000)
+        ev3.light.on(Color.RED)
+        #wait(5000)
+        client_parked = False
+        while not client_parked:
+            msg_from_client = mbox.read()
+            last_msg = msg_from_client
+            if last_msg == 'parked':
+                client_parked = True
+            wait(1000)
+        
+        mbox.send('both_parked')
+        ev3.light.on(Color.YELLOW)
+        wait(random.randint(1, 7)*1000)
+        mbox.send('unpark')
+        wait(7000)
         drive_over_line(color_line, color_base, right_light, (-120, -170))
+        ev3.light.on(Color.GREEN)
+        return True
     else:
         drive_robot((0, 150))
         wait(200)
+        return False
 
 
 def calibrate():
@@ -114,16 +133,32 @@ def light_on_line(color_line, light):
     return abs(ref-color_line) < 2
 
 
+def connect():
+    server = BluetoothMailboxServer()
+    mbox = TextMailbox('greeting', server)
+    print("Waiting for connection..")
+    server.wait_for_connection()
+    print("Connected..")
+    return mbox
+
+
 def main():
     """Main Function"""
+    ev3.light.on(Color.GREEN)
     color_left, color_right = calibrate()
-
-    base_velocity = 400
+    mbox = connect()
+    
+    parking_enabled = False
+    
+    last_msg = mbox.read()
+    
+    base_velocity = 170
     steering_offset = 1
 
-    drive_over_line(color_left, color_right, right_light, (180, 180))
+    drive_to_line(color_left, color_right, right_light, (180,180))
+    #drive_over_line(color_left, color_right, right_light, (180, 180))
     timer = time.time()
-
+    
     while True:
         distance = obstacle_sensor.distance()
         velocity = base_velocity*min(1, ((distance-100)/200))
@@ -131,10 +166,27 @@ def main():
         vel = velocity_fn(norm(color_left, color_right, right_light.reflection()), velocity, steering_offset)
         drive_robot(vel)
         
-        if light_on_line(color_left, left_light) and time.time()-timer > 1.7:
-            parking_mode(color_left, color_right)
+        if parking_enabled and light_on_line(color_left, left_light) and time.time()-timer > 1.6:
+            parked = parking_mode(color_left, color_right, mbox)
             timer = time.time()
-
+            parking_enabled = not parked
+        
+        if time.time() - timer > 15 and not parking_enabled:
+            print("sending parking")
+            mbox.send('park')
+            mbox.send('park')
+            mbox.send('park')
+            mbox.send('park')
+            mbox.send('0')
+            parking_enabled = True
+        
+        msg_from_client = mbox.read()
+        if msg_from_client != last_msg:
+            print(msg_from_client)
+            last_msg = msg_from_client
+            if last_msg == 'parked':
+                client_parked = True
+                
 
 if __name__ == '__main__':
     main()
